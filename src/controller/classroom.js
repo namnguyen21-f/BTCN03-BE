52dc0b6aac85486482a1bb88e5fe00eb6403a6c7
Nguyen Le Hoang Nam <nam.nguyen@popsww.com>
2021-11-15 11:44:56 +0700

    Change Url project link

--- src/controller/classroom.js
+++ src/controller/classroom.js
@@ -185,7 +185,7 @@ exports.sendInvitationLink = (req,res) => {
                         })
                     }else{
                         const to = req.body.email;
-                        const link = 'https://btcn03-18127160.herokuapp.com/api/class' + req.params.id + '/invite/' + data._id;
+                        const link = 'https://midtermproject160220.herokuapp.com/api/class' + req.params.id + '/invite/' + data._id;
                         var mailOptions = {
                             to: to,
                             subject: "Email for inviting user to class",
@@ -219,7 +219,7 @@ exports.sendInvitationLink = (req,res) => {
 
 exports.generateLink = (req,res) => {
     if (req.user){
-        const link = 'https://btcn03-18127160.herokuapp.com/api/class/' + req.params.id + '/invite/url';
+        const link = 'https://midtermproject160220.herokuapp.com/api/class/' + req.params.id + '/invite/url';
         return res.status(200).json({
             data : link,
         }) 
