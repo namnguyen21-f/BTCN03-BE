da375eaf6c15d6a2c5cfef7a2a9b3d7db97d9bb5
Nguyen Le Hoang Nam <nam.nguyen@popsww.com>
2021-11-16 22:40:24 +0700

    Update classroom.js

--- src/controller/classroom.js
+++ src/controller/classroom.js
@@ -186,7 +186,7 @@ exports.sendInvitationLink = (req,res) => {
                         })
                     }else{
                         const to = req.body.email;
-                        const link = 'https://midtermproject160220.herokuapp.com/api/class' + req.params.id + '/invite/' + data._id;
+                        const link = 'https://midtermproject160220.herokuapp.com/api/class/' + req.params.id + '/invite/' + data._id;
                         var mailOptions = {
                             to: to,
                             subject: "Email for inviting user to class",
@@ -195,8 +195,10 @@ exports.sendInvitationLink = (req,res) => {
                 
                         smtpTransport.sendMail(mailOptions, function(error, info) {
                             if (error) {
-                                return res.status(400).json({
-                                    err: error,
+                                Invitation.deleteOne({_id: data._id} ,  function (err) {
+                                    return res.status(400).json({
+                                        message: "Request Failed",
+                                    })
                                 })
                             } else {
                                 return res.status(200).json({
